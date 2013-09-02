4e8fa03e48a5b1c04fe335ba6980b1693b6cba63
paulhkim80 <paulhkim80@gmail.com>
2013-09-02 15:25:20 +0900

    read index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buf = new Buffer(fs.readFileSync("index.html"));
-  response.send(Buffer.toString(buf));
+  response.send(buf.toString());
   //response.send('Hello World2!');
 });
 
